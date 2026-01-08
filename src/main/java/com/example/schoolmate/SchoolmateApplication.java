52dd2158b4c79edb6c16e0f1c9d01f5a48769b88
hyeonwoo <lhi654@gmail.com>
2026-01-08 16:24:37 +0900

    settings

---
+++ src/main/java/com/example/schoolmate/SchoolmateApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/schoolmate/SchoolmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
